7e3fb0cf2af6314758024afd8b119bc28bc6c2d1
aldoaquino451 <aldoaquino2909@gmail.com>
2023-11-22 17:11:01 +0100

    [add] classi bootstrap

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -9,7 +9,7 @@ use App\Models\Character;
 class PageController extends Controller
 {
     public function index(){
-        $characters = Character::all();
+        $characters = Character::limit(10)->get();
         return view('home', compact('characters'));
     }
 }
